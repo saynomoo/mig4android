810e9d4a8e894e52657dde0a2f7fe391e5fb588c
Tuomas Huhtanen <tuomas@saynomoo.com>
2011-06-26 23:02:20 +0300

    change separator constraints to the same as in swt demo (was copied from swing demo) as the rest of the demo uses same layout as in swt demo

--- examples/src/com/saynomoo/mig4android/MigActivity.java
+++ examples/src/com/saynomoo/mig4android/MigActivity.java
@@ -23,12 +23,12 @@ public abstract class MigActivity extends Activity {
     public abstract MigLayout createLayout();
 
     void addSeparator(MigLayout migLayout, String label) {
-        TextView textView = addLabel(migLayout, label, "gapbottom 1, span, split 2, aligny center");
+        TextView textView = addLabel(migLayout, label, "gaptop para, span, split 2");
         textView.setTextColor(getResources().getColor(R.color.label));
         View separatorLine = new View(this);
         separatorLine.setMinimumHeight(1);
         separatorLine.setBackgroundColor(getResources().getColor(R.color.separator));
-        migLayout.addView(separatorLine, "gapleft rel, growx");
+        migLayout.addView(separatorLine, "gapleft rel, gaptop para, growx");
     }
 
     void addTextField(MigLayout migLayout, String layoutConstraints) {
