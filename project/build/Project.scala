4c6b16729e41df03ddbc2fccf69936bdd8d58076
Tuomas Huhtanen <tuomas@saynomoo.com>
2011-07-09 23:30:23 +0300

    disable crossPaths, which append scala version to artifact id (not required, since this is java)

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -8,10 +8,12 @@ class Project(info: ProjectInfo) extends ParentProject(info){
   lazy val examples = project("examples", "Mig4Android Examples", new ExamplesProject(_), mig4android)
 
   class Mig4AndroidProject(info: ProjectInfo) extends DefaultProject(info){
+    override def disableCrossPaths = true
     override def unmanagedClasspath = super.unmanagedClasspath +++ (Path.fromFile(System.getenv("ANDROID_SDK_HOME")) / "platforms" / "android-7" / "android.jar")
   }
 
   class ExamplesProject(info: ProjectInfo) extends AndroidProject(info){
+    override def disableCrossPaths = true
     def androidPlatformName = "android-7"
   }
 }
