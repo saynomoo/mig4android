532144bb2d240f6668b71daf549031af442d81da
Tuomas Huhtanen <tuomas@saynomoo.com>
2011-07-10 00:28:08 +0300

    publishable project

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -4,12 +4,16 @@ import java.io.File
 
 class Project(info: ProjectInfo) extends ParentProject(info){
 
-  lazy val mig4android = project("mig4android", "Mig4Android", new Mig4AndroidProject(_))
-  lazy val examples = project("examples", "Mig4Android Examples", new ExamplesProject(_), mig4android)
+  lazy val mig4android = project("mig4android", "mig4android", new Mig4AndroidProject(_))
+  lazy val examples = project("examples", "examples", new ExamplesProject(_), mig4android)
 
   class Mig4AndroidProject(info: ProjectInfo) extends DefaultProject(info){
     override def disableCrossPaths = true
     override def unmanagedClasspath = super.unmanagedClasspath +++ (Path.fromFile(System.getenv("ANDROID_SDK_HOME")) / "platforms" / "android-7" / "android.jar")
+    override def packageSrcJar= defaultJarPath("-sources.jar")
+    val sourceArtifact = Artifact.sources(artifactID)
+    override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageSrc)
+    lazy val publishTo = Resolver.file("GitHub", new File((Path.userHome / "saynomoo.github.com" / "maven").toString))
   }
 
   class ExamplesProject(info: ProjectInfo) extends AndroidProject(info){
