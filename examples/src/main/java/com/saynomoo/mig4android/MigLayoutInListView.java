a27705751aea68efadda41a7b8e4c6d3aa6dd6a5
Tuomas Huhtanen <tuomas@saynomoo.com>
2012-01-10 00:02:04 +0200

    Example using MigLayout in ListView

---
+++ examples/src/main/java/com/saynomoo/mig4android/MigLayoutInListView.java
@@ -0,0 +1,40 @@
+package com.saynomoo.mig4android;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.*;
+
+public class MigLayoutInListView extends MigActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new ListView(this){{
+            setAdapter(new BaseAdapter() {
+                public int getCount() {
+                    return 10;
+                }
+
+                public Object getItem(int position) {
+                    return position;
+                }
+
+                public long getItemId(int position) {
+                    return position;
+                }
+
+                public View getView(final int position, View convertView, ViewGroup parent) {
+                    return new MigLayout(getContext()) {{
+                        addLabel(this, "Item " + position, "");
+                        setLayoutParams(new ListView.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, 30));
+                    }};
+                }
+            });
+        }});
+    }
+
+    @Override
+    public MigLayout createLayout() {
+        return new MigLayout(this);
+    }
+}
