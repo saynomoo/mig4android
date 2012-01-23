72f69dd48f84aede1e9ae7c6ea6c2e0a9e339de3
Tuomas Huhtanen <tuomas@saynomoo.com>
2012-01-23 22:05:56 +0200

    Workaround for issue #7. Better test for Logical pixels
    The workaround is to put <uses-sdk android:minSdkVersion="4" /> to the Manifest.xml

--- examples/src/main/java/com/saynomoo/mig4android/test/LogicalPixels.java
+++ examples/src/main/java/com/saynomoo/mig4android/test/LogicalPixels.java
@@ -2,6 +2,8 @@ package com.saynomoo.mig4android.test;
 
 import com.saynomoo.mig4android.MigActivity;
 import com.saynomoo.mig4android.MigLayout;
+import com.saynomoo.mig4android.ViewGroupWrapper;
+import net.miginfocom.layout.PlatformDefaults;
 
 public class LogicalPixels extends MigActivity {
     @Override
@@ -11,8 +13,21 @@ public class LogicalPixels extends MigActivity {
     class LogicalPixelsView extends MigLayout {
         public LogicalPixelsView() {
             super(LogicalPixels.this);
+            setLayoutConstraints("wrap 2");
+            addLabel(this, getResources().getDisplayMetrics().toString(), "span 2");
+            final ViewGroupWrapper w = getParentWrapper();
+            final int defaultDPI = PlatformDefaults.getDefaultDPI();
+            final String label = "ViewWrapper: " + "{density=" + ((float)w.getHorizontalScreenDPI()) / defaultDPI + ", width=" + w.getScreenWidth() +
+                    ", height=" + w.getScreenHeight() + ", scaledDensity=" + '?' +
+                    ", xdpi=" + w.getHorizontalScreenDPI() + ", ydpi=" + w.getVerticalScreenDPI() + "}, default DPI: " + defaultDPI;
+            addLabel(this, label, "span 2");
             addConstraintLabel(this, "height 100px");
             addConstraintLabel(this, "height 100");
+
+            final String widthPx = "width " + w.getScreenWidth()/2 + "px";
+            addLabel(this, widthPx, widthPx + ", wrap");
+            final String widthDp = "width " + defaultDPI * (w.getScreenWidth()/2) / w.getHorizontalScreenDPI();
+            addLabel(this, widthDp, widthDp + ", wrap");
         }
     }
 }
