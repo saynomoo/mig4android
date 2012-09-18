f6741df875bb3f7529057f680abee3b613d7d7a2
Tuomas Huhtanen <tuomas@saynomoo.com>
2012-09-18 22:47:02 +0300

    Add missing class MigLayoutFromXml

---
+++ examples/src/main/java/com/saynomoo/mig4android/MigLayoutFromXml.java
@@ -0,0 +1,11 @@
+package com.saynomoo.mig4android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MigLayoutFromXml extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }}
